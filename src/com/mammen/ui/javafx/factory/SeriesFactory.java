29102b95ee771b91b4b5b6581141f4496805818c
rlee287 <rlee287@yahoo.com>
2018-08-01 14:26:28 -0700

    Use a time accumulator when generating velocity series
    Do not assume uniform dt as that won't be true anymore

--- src/com/mammen/ui/javafx/factory/SeriesFactory.java
+++ src/com/mammen/ui/javafx/factory/SeriesFactory.java
@@ -24,12 +24,14 @@ public class SeriesFactory
 
     public static XYChart.Series<Double, Double> buildVelocitySeries(Trajectory t) {
         XYChart.Series<Double, Double> series = new XYChart.Series<>();
-
+        double time_accumulator=0;
         for (int i = 0; i < t.segments.length; i++) {
             XYChart.Data<Double, Double> data = new XYChart.Data<>();
 
-            data.setXValue(t.get(i).dt * i);
+            data.setXValue(time_accumulator);
             data.setYValue(t.get(i).velocity);
+            
+            time_accumulator+=t.get(i).dt;
 
             series.getData().add(data);
         }
